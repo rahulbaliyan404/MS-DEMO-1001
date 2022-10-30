c256935d70e06c288fb5da29390f2c1a78e8f71e
rahulbaliyan@gmail.com <rahulbaliyan@gmail.com>
2022-10-30 13:13:42 +0530

    This is demo code for encoder and decoder password.

--- MS-Demo-1001/src/main/java/com/home/MsDemo1001Application.java
+++ MS-Demo-1001/src/main/java/com/home/MsDemo1001Application.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class MsDemo1001Application {
-// This is MS-Demo-1001 application main method. 
 	public static void main(String[] args) {
 		SpringApplication.run(MsDemo1001Application.class, args);
 	}
